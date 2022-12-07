fb71a4ce3940f8f846f0d78b265ba4c3ee3b2d5b
Decwest <fumiyaonishi1016@gmail.com>
2022-12-07 19:52:24 +0900

    :sparkles: install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+# git config
+git config --global commit.template ~/dotfiles/.commit_template
+
+# aliases
+source ~/dotfiles/.aliases
+echo "source ~/dotfiles/.aliases" >> ~/.bashrc
+
+# install tools
+source ~/dotfiles/install/terminator.sh
+source ~/dotfiles/install/chrome.sh
+source ~/dotfiles/install/vscode.sh
+source ~/dotfiles/install/docker.sh
+source ~/dotfiles/install/nvidia_docker.sh
+source ~/dotfiles/install/ros.sh
---
+++ install/terminator.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install terminator
+cp -r ~/dotfiles/.config/terminator ~/.config/terminator
