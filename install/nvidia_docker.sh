53d573d4b23fe1a7841bf9705ac2e19f5f337442
Decwest <fumiyaonishi1016@gmail.com>
2023-08-23 20:21:50 +0900

    :zap: update nvidia docker installation

--- install/nvidia_docker.sh
+++ install/nvidia_docker.sh
@@ -1,12 +1,12 @@
 # Add nvidia-docker's GPG keys
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
       && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 # Install nvidia-docker
-sudo apt-get update && sudo apt-get install -y nvidia-docker2
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 
 # Restart docker
 sudo systemctl restart docker
