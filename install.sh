5b826e94ed11a3bcbb7a575f5c88336cf5acd923
Decwest <fumiyaonishi1016@gmail.com>
2024-08-13 22:26:00 +0900

    :bug: delete comment out

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-# # git config
-# git config --global commit.template ~/dotfiles/.commit_template
+# git config
+git config --global commit.template ~/dotfiles/.commit_template
 
-# # aliases
-# source ~/dotfiles/.aliases
-# echo "source ~/dotfiles/.aliases" >> ~/.bashrc
+# aliases
+source ~/dotfiles/.aliases
+echo "source ~/dotfiles/.aliases" >> ~/.bashrc
 
-# # install tools
-# source ~/dotfiles/install/mxergo_shortcut.sh
-# source ~/dotfiles/install/terminator.sh
-# source ~/dotfiles/install/chrome.sh
-# source ~/dotfiles/install/vscode.sh
-# source ~/dotfiles/install/docker.sh
-# source ~/dotfiles/install/nvidia_docker.sh
+# install tools
+source ~/dotfiles/install/mxergo_shortcut.sh
+source ~/dotfiles/install/terminator.sh
+source ~/dotfiles/install/chrome.sh
+source ~/dotfiles/install/vscode.sh
+source ~/dotfiles/install/docker.sh
+source ~/dotfiles/install/nvidia_docker.sh
 
 # Ubuntuのバージョンを取得
 ubuntu_version=$(lsb_release -r | awk '{print $2}')
